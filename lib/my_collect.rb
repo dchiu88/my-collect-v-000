260342cef5e4fdef33bd3a24e1daf691923678de
David Chiu <davidwchiu@gmail.com>
2018-03-29 00:54:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,14 @@
-my_collect(student) do |name| 
-  name.split(" ").first 
+def my_collect(array)
+  new_arr = [] 
+  
+  i=0 
+  while i < array.length 
+  new_arr.push yield(array[i])
+  i += 1 
 end 
-
-my_collect(collection) do |lang|
-  lang.upcase 
+  # for element in array 
+  #   new_arr.push yield element 
+  # end 
+  
+  new_arr
 end 
-
